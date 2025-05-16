5faf86f95eff408b75e0885aa00453b4b6627fd8
PolMarsolUdl <144933485+polMarsol@users.noreply.github.com>
2025-05-16 09:30:35 +0200

    Delete app directory

--- app/src/main/java/com/example/roamly/domain/models/Preferences.kt
+++
@@ -1,8 +0,0 @@
-package com.example.roamly.domain.models
-
-data class Preferences(
-    val id: Int,
-    val notificationsEnabled: Boolean,
-    val preferredLanguage: String,
-    val theme: String
-)
--- app/src/main/java/com/example/roamly/domain/repository/MapRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.roamly.domain.repository
-import com.example.roamly.domain.models.Map
-
-interface MapRepository {
-    fun getCurrentLocation(): Map?
-    fun getNearbyPlaces(latitude: Double, longitude: Double): List<Map>
-}
